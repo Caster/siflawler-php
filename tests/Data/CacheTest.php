441719aaa8f9758e5151e8925d55528310af8fee
Thom Castermans <thomcastermans@users.sourceforge.net>
2015-01-22 16:20:28 +0100

    Fix 'rijdendetreinen' test.
    The test was depended on data that would change (namely, the number of pages).
    Due to a better query, it now is not anymore.

--- tests/Data/CacheTest.php
+++ tests/Data/CacheTest.php
@@ -13,15 +13,14 @@ class CacheTest extends \PHPUnit_Framework_TestCase {
 
     public function testCrawlMulti() {
         $crawler = new Crawler(TestCache::$config_file_rijdendetreinen);
-        list($data, $visited) = $crawler->crawl(
-            'http://www.rijdendetreinen.nl/storingen/p593', true);
+        list($data, $visited) = $crawler->crawl(null, true);
         $this->assertInternalType('array', $data);
         $this->assertEquals(150, count($data));
         // Note: without the cache, some pages would be parsed more than once
         //       and we would have had way more elements in the results... but
         //       many of them would be the same.
         $this->assertInternalType('array', $visited);
-        $this->assertEquals(14, count($visited));
+        $this->assertEquals(13, count($visited));
         $visited_expected = array(
             'http://www.rijdendetreinen.nl/storingen/p550',
             'http://www.rijdendetreinen.nl/storingen/p551',
@@ -35,8 +34,7 @@ class CacheTest extends \PHPUnit_Framework_TestCase {
             'http://www.rijdendetreinen.nl/storingen/p559',
             'http://www.rijdendetreinen.nl/storingen/p560',
             'http://www.rijdendetreinen.nl/storingen/p561',
-            'http://www.rijdendetreinen.nl/storingen/p562',
-            'http://www.rijdendetreinen.nl/storingen/p593'
+            'http://www.rijdendetreinen.nl/storingen/p562'
         );
         for ($i = 0; $i < 10; $i++) {
             $this->assertEquals($visited_expected[$i], $visited[$i]);
